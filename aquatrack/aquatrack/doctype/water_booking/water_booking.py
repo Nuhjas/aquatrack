306deea4da97f5b8a055df5a4605801f8d8aaa9f
Nuhjas <nuhjas11@gmail.com>
2025-08-21 08:27:13 +0000

    Lead created

--- aquatrack/aquatrack/doctype/water_booking/water_booking.py
+++ aquatrack/aquatrack/doctype/water_booking/water_booking.py
@@ -1,9 +1,51 @@
 # Copyright (c) 2025, nj and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class WaterBooking(Document):
-	pass
+    def validate(self):
+
+        pass
+
+    def after_insert(self):
+
+        if self.number:
+            customer = frappe.db.get_value(
+                "Customer", {"mobile_no": self.number}, "name"
+            )
+            if not customer:
+                lead_name = f"booking_lead {frappe.db.count('Lead')+1}"
+                lead = frappe.get_doc(
+                    {
+                        "doctype": "Lead",
+                        "first_name": lead_name,
+                        "mobile_no": self.number,
+                        "status": "Open",
+                    }
+                )
+                lead.insert(ignore_permissions=True)
+                frappe.msgprint("Lead Created")
+
+                return
+
+            s_o = frappe.get_doc(
+                {
+                    "doctype": "Sales Order",
+                    "customer": customer,
+                    "transaction_date": frappe.utils.nowdate(),
+                    "delivery_date": frappe.utils.nowdate(),
+                    "items": [],
+                }
+            )
+
+            for row in self.items:
+                s_o.append(
+                    "items",
+                    {"item_code": row.item, "qty": row.quantity or 1, "rate": 100},
+                )
+
+            s_o.insert(ignore_permissions=True)
+            frappe.msgprint("sales order created")
--- aquatrack/aquatrack/web_form/water/water.js
+++ aquatrack/aquatrack/web_form/water/water.js
@@ -3,20 +3,7 @@
 frappe.ready(function () {
     frappe.web_form.validate = () =>  {
         let phone = frappe.web_form.doc.number;
-        let items_list = frappe.web_form.get_values().items || [];
-        console.log(items_list)
-        
-        let so_items = [];
-        items_list.forEach(row => {
-            so_items.push({
-                item_code:row.item,
-                qty: row.quantity || 1,
-                rate: 100,
-
-            })
-        
-            
-        });// ensure "number" is the exact fieldname
+       
 
         // Remove previous error messages
         $('.phone-error').remove();
@@ -29,49 +16,8 @@ frappe.ready(function () {
 
             return false; // prevent submission
         }
-        frappe.call({
-            method: "frappe.client.get_list",
-            args: {
-                doctype: "Customer",
-                fields: ["name", "customer_name"],
-                filters: { mobile_no: phone },
-                limit: 1
-            },
-            callback: function (r) {
-                if (r.message && r.message.length > 0) {
-                    //frappe.msgprint("hoii");
-                    let customer_n = r.message[0].name;
-                    frappe.call({
-                        method: "frappe.client.insert",
-                        args: {
-                            doc: {
-                                doctype: "Sales Order",
-                                customer: customer_n,
-                                transaction_date: frappe.datetime.get_today(),
-                                delivery_date: frappe.datetime.get_today(),
-                                items: so_items
-                                
-                            }
-                        },
-                        callback: function (res) {
-                            if (res.message) {
-                                frappe.msgprint(`✅ Sales Order <b>${res.message.name}</b> created for Customer ${customer}`);
-                            }
-                        }
-                    });
-
-
-                } else {
-                    frappe.msgprint("⚠️ No Customer found with this phone number. Please register first.");
-                }
-
-            }
-        });
-
-
-
-
-        return false;
+        
+        return true;
 
         //allow save to continue
     };
